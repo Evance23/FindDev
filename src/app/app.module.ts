3079cb3a268811917d845cadc54f8986bb3635a5
Evance23 <evancebarracks01@gmail.com>
2021-04-06 19:05:33 +0300

    Add changes to components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ServicesService } from './services.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+// import { LandingComponent } from './landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
 
 
@@ -14,10 +15,10 @@ import { ProfileComponent } from './profile/profile.component';
   declarations: [
     AppComponent,
     LandingComponent,
-    ProfileComponent
-    HttpModule,
+    ProfileComponent,
+    HttpClientModule,
   ],
-  
+
   imports: [
     BrowserModule,
     AppRoutingModule
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import 'rxjs/add/operator/maps';
 
 
@@ -9,15 +9,17 @@ import 'rxjs/add/operator/maps';
 })
 export class ServicesService {
 
-private:username: string;
-private: clientid='077081e090818fb1cc4b';
-private: clientsecret= 'fe57832ff033be3763be5269c372231634423aec'; 
-  constructor(private http: Http) { 
+private username: string;
+private clientid='077081e090818fb1cc4b';
+private clientsecret= 'fe57832ff033be3763be5269c372231634423aec'; 
+  constructor(private http: HttpClient) { 
     console.log("service ready")
     this.username='Evance23';
   }
 getProfile(){
-return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" this.clientid + "&client_secret=" this.clientsecret)
-.map(res=>res.json());
+return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret="  + this.clientsecret)
+
+
+.map((res:any)=>res.json());
 }
 }
